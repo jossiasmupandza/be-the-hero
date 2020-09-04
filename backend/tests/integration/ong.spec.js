f38c2905049e75cd319029c6cf8e007c41c98a8b
jossias <mupandzaj@gmail.com>
2020-09-04 17:35:11 +0200

    adds integrated tests for all routes

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -3,7 +3,9 @@ const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
-    beforeEach(async () => {
+    var ongId;
+
+    beforeAll(async () => {
         await connection.migrate.rollback();
         await connection.migrate.latest();
     })
@@ -16,14 +18,37 @@ describe('ONG', () => {
         const response = await request(app)
             .post("/ongs")
             .send({
-            name: "Cantinho dos gatos",
-            email: "gatos@gmai.com",
-            whatsapp: "847109372",
-            city: "Sofalas",
-            uf: "Sf",
-        });
+                name: "Cantinho dos gatos",
+                email: "gatos@gmail.com",
+                whatsapp: "847109371",
+                city: "Sofalas",
+                uf: "SF",
+            });
 
         expect(response.body).toHaveProperty('id');
         expect(response.body.id).toHaveLength(8);
-    })
+    });
+
+    it('Should be able to List ONGs', async () => {
+        const response = await request(app).get("/ongs");
+
+        expect(response.body[0]).toHaveProperty('id');
+        expect(response.body[0]).toHaveProperty('name', 'Cantinho dos gatos');
+        expect(response.body[0]).toHaveProperty('email', 'gatos@gmail.com');
+        expect(response.body[0]).toHaveProperty('whatsapp', '847109371');
+        expect(response.body[0]).toHaveProperty('city', 'Sofalas');
+        expect(response.body[0]).toHaveProperty('uf', 'SF');
+
+        ongId = response.body[0].id;
+    });
+
+    it('should be able to create a session', async () => {
+        const response = await request(app)
+            .post('/session')
+            .send({
+                id: ongId
+            });
+
+        expect(response.body).toHaveProperty('name', 'Cantinho dos gatos');
+    });
 })
